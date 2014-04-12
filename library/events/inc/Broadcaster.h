b383576679573ec5e11e2702f27808c9bfe5e8af
shak-mar <shakmar@openmailbox.org>
2014-04-12 14:26:42 +0200

    Added Broadcaster
    The Broadcaster is a class that implements a simple distribution
    mechanism for events. It extends and implements both the event::Listener
    and the event::Service interface. Basically, you can subscribe Listeners
    to it that will later get the events the Broadcaster gets.
    This is useful because most classes that provide a service would
    probably implement it in about the same way, so the implementation can
    be decoupled here.

---
+++ library/events/inc/Broadcaster.h
@@ -0,0 +1,44 @@
+#ifndef GUARD_EVENT_BROADCASTER
+#define GUARD_EVENT_BROADCASTER
+
+#include <vector>
+#include <algorithm>
+
+#include "Listener.h"
+#include "Service.h"
+
+namespace event {
+    template < typename Event_type >
+    class Broadcaster : public Listener<Event_type>, public Service<Event_type> {
+        public:
+            virtual ~Broadcaster<Event_type> () = default;
+
+            virtual void subscribe (Listener<Event_type>& listener);
+            virtual void de_subscribe (Listener<Event_type>& listener);
+
+            virtual void handle_event (Event_type& event);
+
+        protected:
+            std::vector<Listener<Event_type>*> subscriptions;
+    };
+
+    template < typename T >
+    void Broadcaster<T>::subscribe (Listener<T>& listener) {
+        subscriptions.push_back(&listener);
+    }
+
+    template < typename T >
+    void Broadcaster<T>::de_subscribe (Listener<T>& listener) {
+        subscriptions.erase(std::remove(subscriptions.begin(), subscriptions.end(), &listener), subscriptions.end());
+    }
+
+    template < typename T >
+    void Broadcaster<T>::handle_event (T& event) {
+        std::for_each(subscriptions.begin(), subscriptions.end(),
+                [&](Listener<T>* listener) { listener->handle_event(event); });
+    }
+
+}
+
+
+#endif // GUARD_EVENT_BROADCASTER

eba4991d75a5c3ad94ef654630d6ac7f80d39cfb
shak-mar <shakmar@openmailbox.org>
2014-04-17 14:19:08 +0200

    Add Emitter Pure Abstract Class to events
    The Emitter PAC is not much different from Service, it just doesn't
    allow for multiple Listeners. This is useful if you want to swap between
    two lists of different recipients, you can just swap out one Broadcaster
    with another.

---
+++ library/events/inc/Emitter.h
@@ -0,0 +1,16 @@
+#ifndef GUARD_EVENT_EMITTER
+#define GUARD_EVENT_EMITTER
+
+#include "Listener.h"
+
+namespace events {
+    template < typename Event_type >
+    class Emitter {
+        public:
+            virtual ~Emitter<Event_type> () = default;
+
+            virtual void set_output (Listener<Event_type>& listener) = 0;
+    };
+}
+
+#endif // GUARD_EVENT_EMITTER

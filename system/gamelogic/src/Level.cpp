6afbe5675766599dc8316e1d68aa181a9d160b7b
shak-mar <shakmar@openmailbox.org>
2014-04-24 15:50:40 +0200

    Corrected placement of Level::set_tile function
    The Level::set_tile function is declared before the Level::get_tile_at
    function in system/gamelogic/src/Level.h. Declaration and definition of
    the member functions should be in the same order, though.

--- system/gamelogic/src/Level.cpp
+++ system/gamelogic/src/Level.cpp
@@ -2,17 +2,17 @@
 
 using namespace gamelogic;
 
+void Level::set_tile (std::shared_ptr<Tile> tile) {
+    remove_tile_at(tile->get_position());
+    tiles.push_back(tile);
+}
+
 Tile* Level::get_tile_at (geom2d::Vector<int> position) {
     auto at = std::find_if (tiles.begin(), tiles.end(),
             [&](std::shared_ptr<Tile> t){return t->get_position() == position;});
     if (at == tiles.end())
         return nullptr;
     return at->get();
-}
-
-void Level::set_tile (std::shared_ptr<Tile> tile) {
-    remove_tile_at(tile->get_position());
-    tiles.push_back(tile);
 }
 
 void Level::remove_tile_at (geom2d::Vector<int> position) {
